4cc2876b896e86460054ae3cc33a04f4e83db4bc
heidarianw <william.heidarian@credera.com>
2019-07-29 13:47:54 -0600

    added empty spring boot application

---
+++ horizons-proj/src/main/java/ots/andy/group/horizonsproj/HorizonsProjApplication.java
@@ -0,0 +1,13 @@
+package ots.andy.group.horizonsproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonsProjApplication.class, args);
+	}
+
+}
---
+++ horizons-proj/src/test/java/ots/andy/group/horizonsproj/HorizonsProjApplicationTests.java
@@ -0,0 +1,16 @@
+package ots.andy.group.horizonsproj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HorizonsProjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
