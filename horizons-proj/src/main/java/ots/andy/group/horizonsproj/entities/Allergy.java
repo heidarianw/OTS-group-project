4d63ecf3440f361af4cc21cb4281c58b6ee92d48
Lena Wayson <lena.wayson@LWayson-MBP.local>
2019-07-30 16:04:09 -0600

    allergies entity

---
+++ horizons-proj/src/main/java/ots/andy/group/horizonsproj/entities/Allergy.java
@@ -0,0 +1,31 @@
+package ots.andy.group.horizonsproj.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="allergy")
+public class Allergy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "allergy")
+    private String allergy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAllergy() {
+        return allergy;
+    }
+
+    public void setAllergy(String allergy) {
+        this.allergy = allergy;
+    }
+}
