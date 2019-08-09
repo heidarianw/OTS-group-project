2a6d09352604fe56781c1a2632e2e88cc734a998
Clay <clay.kramp@credera.com>
2019-08-09 09:28:07 -0600

    Added daycare and personality tests

---
+++ horizons-proj/src/test/java/ots/andy/group/horizonsproj/services/DaycareControllerTest.java
@@ -0,0 +1,53 @@
+package ots.andy.group.horizonsproj.services;
+
+import org.apache.coyote.Response;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ots.andy.group.horizonsproj.controllers.AllergyController;
+import ots.andy.group.horizonsproj.controllers.DaycareController;
+import ots.andy.group.horizonsproj.entities.Allergy;
+import ots.andy.group.horizonsproj.entities.Daycare;
+import ots.andy.group.horizonsproj.repositories.AllergyRepository;
+import ots.andy.group.horizonsproj.repositories.DaycareRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+public class DaycareControllerTest {
+
+    private DaycareController controller;
+
+    private DaycareService service = Mockito.mock(DaycareService.class);
+    Daycare d = new Daycare ("Denver");
+    List<Daycare> list = new ArrayList<Daycare>() {
+        {
+            add(d);
+        }
+    };
+
+    @BeforeEach public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @BeforeEach
+    public void init()
+    {
+        controller = new DaycareController(service);
+    }
+
+    @Test
+    public void responseTest() {
+        when(service.getAllDaycare()).thenReturn(list);
+        ResponseEntity<Daycare> response = controller.getAllDaycare();
+        verify(service, times(1)).getAllDaycare();
+        ResponseEntity<Daycare> out = new ResponseEntity(list, HttpStatus.OK);
+        assertTrue(out.equals( response));
+    }
+}
---
+++ horizons-proj/src/test/java/ots/andy/group/horizonsproj/services/PersonalityControllerTest.java
@@ -0,0 +1,56 @@
+package ots.andy.group.horizonsproj.services;
+
+import org.apache.coyote.Response;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import ots.andy.group.horizonsproj.controllers.AllergyController;
+import ots.andy.group.horizonsproj.controllers.DaycareController;
+import ots.andy.group.horizonsproj.controllers.PersonalityController;
+import ots.andy.group.horizonsproj.entities.Allergy;
+import ots.andy.group.horizonsproj.entities.Daycare;
+import ots.andy.group.horizonsproj.entities.Personality;
+import ots.andy.group.horizonsproj.repositories.AllergyRepository;
+import ots.andy.group.horizonsproj.repositories.DaycareRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+public class PersonalityControllerTest {
+
+    private PersonalityController controller;
+
+    private PersonalityService service = Mockito.mock(PersonalityService.class);
+    Personality d = new Personality ("Denver");
+    List<Personality> list = new ArrayList<Personality>() {
+        {
+            add(d);
+        }
+    };
+
+    @BeforeEach public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @BeforeEach
+    public void init()
+    {
+        controller = new PersonalityController(service);
+    }
+
+    @Test
+    public void responseTest() {
+        when(service.getAllPersonality()).thenReturn(list);
+        ResponseEntity<Personality> response = controller.getAllPersonality();
+        verify(service, times(1)).getAllPersonality();
+        ResponseEntity<Personality> out = new ResponseEntity(list, HttpStatus.OK);
+        assertTrue(out.equals( response));
+    }
+}
